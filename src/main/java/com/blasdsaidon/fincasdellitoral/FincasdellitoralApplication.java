ae8f31453604c459218a7278ab835c8be23a98c8
Blas <blasd.saidon@gmail.com>
2023-11-14 16:41:02 -0300

    primer commit

---
+++ src/main/java/com/blasdsaidon/fincasdellitoral/FincasdellitoralApplication.java
@@ -0,0 +1,13 @@
+package com.blasdsaidon.fincasdellitoral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincasdellitoralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincasdellitoralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blasdsaidon/fincasdellitoral/FincasdellitoralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blasdsaidon.fincasdellitoral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincasdellitoralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
